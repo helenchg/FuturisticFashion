d36b23d5ce8f7fa9c3a3eb44def8f35e25c78027
Elena <chonge@rose-hulman.edu>
2015-02-28 17:23:12 -0500

    trial test

--- raspi/ledtest.py
+++ raspi/ledtest.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 # Setup GPIO using board numbering
 GPIO.setmode(GPIO.BOARD)
@@ -10,15 +11,21 @@ BLUE = 17
 
 # Main function
 GPIO.setup(RED, GPIO.output) #set pin 11 as output
+GPIO.output(RED, 1)
 GPIO.setup(GREEN, GPIO.output) #set pin 13 as output
+GPIO.output(GREEN, 1)
 GPIO.setup(BLUE, GPIO.output) #set pin 17 as output
+GPIO.output(BLUE, 1)
 
-pr = GPIO.PWM(RED, 50)
-pg = GPIO.PWM(GREEN, 0)
-pb = GPIO.PWM(BLUE, 0)
+try:
+	while(True):
+		### Simple RGB LED Strip test
+		request = raw_input("RGB: ")
+		if (len(request) ==3):
+			GPIO.output(RED, int(request[0])) # storing the first digit 
+			GPIO.output(GREEN, int(request[1])) # storing the second digit 
+			GPIO.output(BLUE, int(request[2])) # storing the third digit 
 
-pr.start(0);
-
-
-GPIO.cleanup()
+except KeyboardInterrupt:
+	GPIO.cleanup()
 
