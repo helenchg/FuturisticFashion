04f583d11ec5283e8429391ac2c080a4d99e53ff
munchie945 <manc@rose-hulman.edu>
2015-03-01 10:47:35 -0700

    Fixed calling interval within PinchColor

--- raspi/main.py
+++ raspi/main.py
@@ -12,42 +12,55 @@ modes.append(dataProcessors.WhiteOrBlue())
 nModes = len(modes)
 modeIdx = 0;
 currMode = modes[modeIdx]
+print modes[0]
+print modes[1]
+modeIdx = (modeIdx + 1) % nModes
+currMode = modes[modesIdx
+
+modeIdx = (modeIdx + 1) % nModes
+currMode = modes[modesIdx]
+
+modeIdx = (modeIdx + 1) % nModes
+currMode = modes[modesIdx]
+
+modeIdx = (modeIdx + 1) % nModes
+currMode = modes[modesIdx]
 
 ledUtils.init_LEDs()
 
 # if grabStrength > threshold for changeModeThreshold, change mode
 changeModeThreshold = 8
-grabThreshold = 0.6
+grabThreshold = 0.9
 grabCounter = 0
 
-while True:
+# while True:
 
-	try:
-		response = urllib.urlopen(url).read()
+	# try:
+		# response = urllib.urlopen(url).read()
 		# print response
-		data = json.loads(response)['bigdata']
-		print data
+		# data = json.loads(response)['bigdata']
+		# print data
 		
-		if data['grabstrength'] > grabThreshold:
-			grabCounter += 1
-			if grabCounter >= changeModeThreshold:
+		# if data['grabstrength'] > grabThreshold:
+			# grabCounter += 1
+			# if grabCounter >= changeModeThreshold:
 				# fist held for long enough, change mode
-				modeIdx = (modeIdx + 1) % nModes
-				print modeIdx, modes
-				currMode = modes[modesIdx]
-				grabCounter = 0
-				print 'Entering mode: ', currMode.name
+				# modeIdx = (modeIdx + 1) % nModes
+				# print modeIdx, modes
+				# currMode = modes[modesIdx]
+				# grabCounter = 0
+				# print 'Entering mode: ', currMode.name
 		
-		if data['handcount'] == 0:
-			print 'No hands were detected. Please try again.'
-		else:
-			currMode.process(data)
+		# if data['handcount'] == 0:
+			# print 'No hands were detected. Please try again.'
+		# else:
+			# currMode.process(data)
 
-	except KeyboardInterrupt:
-		break
-	except:
-		print 'An error occured'
-		continue
+	# except KeyboardInterrupt:
+		# break
+	# except:
+		# print 'An error occured'
+		# continue
 
 # clean up
 ledUtils.cleanUp()
