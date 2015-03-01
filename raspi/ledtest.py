9fc8bee5ec3197483369f7a581074ae15b55b79f
Elena <chonge@rose-hulman.edu>
2015-02-28 22:38:58 -0500

    a

--- raspi/ledtest.py
+++ raspi/ledtest.py
@@ -9,7 +9,7 @@ GPIO.setwarnings(False)
 RED = 17
 GREEN = 27
 BLUE = 22
-HERTZ = 50 # this is high enough to not see the LED blinking
+HERTZ = 60 # this is high enough to not see the LED blinking
 time_delay = 0.02 # three seconds delay
 steps = 100
 
@@ -43,12 +43,10 @@ try:
 #			pwmB.start(float(dc))
 #			time.sleep(time_delay)
 			for i in range (steps):				# make LED brighter in 100 steps
-				#dc = pwmR.ChangeDutyCycle(i)
-				pwmR.start(float(pwmR.ChangeDutyCycle(i)))
+				pwmR.ChangeDutyCycle(i)
 				time.sleep(time_delay) 				# every time, on for 20ms. To make sure the LED has enough time to perform change
 			
 			for i in range(steps):				# make LED dimmer in 100 steps
-				#pwmR.ChangeDutyCycle(steps-i)
 				pwmR.start(float(pwmR.ChangeDutyCycle(steps-i)))
 				time.sleep(time_delay)
 
