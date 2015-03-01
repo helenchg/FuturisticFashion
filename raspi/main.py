f1355c77a3b52897a39ed2f831bdffda4e0537e1
munchie945 <manc@rose-hulman.edu>
2015-03-01 06:37:27 -0700

    made color an int

--- raspi/main.py
+++ raspi/main.py
@@ -10,29 +10,29 @@ ledUtils.init_LEDs()
 while True:
 	try:
 		response = urllib.urlopen(url).read()
-		print response
+		# print response
 		
-		# data = json.loads(response)['data']
-		# color = data['brightness'] / interval
-		# print color
-		# redOn = color / 4
-		# greenOn = (color % 4) / 2
-		# blueOn = (color % 2)
+		data = json.loads(response)['data']
+		color = int(data['brightness'] / interval)
+		print color
+		redOn = color / 4
+		greenOn = (color % 4) / 2
+		blueOn = (color % 2)
 		
-		# if redOn:
-			# ledUtils.turnRedOn()
-		# else:
-			# ledUtils.turnRedOff()
+		if redOn:
+			ledUtils.turnRedOn()
+		else:
+			ledUtils.turnRedOff()
 			
-		# if greenOn:
-			# ledUtils.turnGreenOn()
-		# else:
-			# ledUtils.turnGreenOff()
+		if greenOn:
+			ledUtils.turnGreenOn()
+		else:
+			ledUtils.turnGreenOff()
 			
-		# if blueOn:
-			# ledUtils.turnBlueOn()
-		# else:
-			# ledUtils.turnBlueOff()
+		if blueOn:
+			ledUtils.turnBlueOn()
+		else:
+			ledUtils.turnBlueOff()
 			
 	except KeyboardInterrupt:
 		break
